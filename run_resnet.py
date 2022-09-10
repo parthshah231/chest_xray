bd3129a94985196cfe30ad98d31974ea98a56a86
Parth Shah <parthshah231@gmail.com>
2022-09-10 15:53:09 -0300

    add trainer from trainer_config_file and remove Config class dependency

--- run_resnet.py
+++ run_resnet.py
@@ -1,6 +1,5 @@
 import json
-from argparse import ArgumentParser
-from gc import callbacks
+from argparse import ArgumentParser, Namespace
 from typing import Dict
 
 import numpy as np
@@ -11,48 +10,30 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score
 from torch.utils.data import DataLoader
 
 from callbacks import get_callbacks
-from config import Config
-from constants import NO_VAL, ROOT
+from constants import ROOT
 from dataloader import ChestXrayDataset
 from resnet import Resnet
 
-# from torchmetrics import Accuracy
-
-
-# Get them from command line!
-# BATCH_SIZE = 32
-# PATCH_SIZE = 256
-# MAX_EPOCHS = 20
-# 3e-4 for smaller batch-size
-# 1e-3 or 1e-4 for bigger batch-size
-# LEARNING_RATE = 3e-4
-# WEIGHT_DECAY = 1e-4
-# N_PATCHES = 11
-# RESNET_VERSION = 18
-# OUT_FEATURES = 1
-
-# random_erasing
-# PROB = 0.45
-# BOX_SIZE = 64
-
-# NO_VAL = True (quick-test)
-# SUBJECT_WISE = True (rescale intenstiy w.r.t. subject)
-
 # For AdamW
 # learning rate proportional to square root of batch_size (theoretically)
+# 3e-4 for smaller batch-size
+# 1e-3 or 1e-4 for bigger batch-size
 
 
 def run_resnet() -> None:
     with open(ROOT / "config.json", "r") as json_file:
         config_dict = json.loads(json_file.read())
-    # with open(ROOT / "trainer_config.json", "r") as json_file:
-    #     trainer_dict = json.loads(json_file.read())
+    with open(ROOT / "trainer_config.json", "r") as json_file:
+        trainer_config_dict = json.loads(json_file.read())
+    trainer_args = Namespace(**trainer_config_dict)
     parser = ArgumentParser()
     parser = Trainer.add_argparse_args(parser)
-    args = parser.parse_args(["--gpus=0", "--max_epochs=20", "--val_check_interval=40"])
+    trainer_args, _ = parser.parse_known_args(namespace=trainer_args)
 
-    # Record parameters for augmentations (random_erasing) as well
+    config_dict["max_epochs"] = trainer_config_dict["max_epochs"]
+
     train_dataset = ChestXrayDataset(
+        config=config_dict,
         phase="train",
         crop=True,
         patch_size=config_dict["patch_size"],
@@ -60,29 +41,28 @@ def run_resnet() -> None:
         prob=config_dict["probability"],
         box_size=config_dict["box_size"],
     )
-    val_dataset = ChestXrayDataset(phase="val", crop=True, patch_size=config_dict["patch_size"])
+    val_dataset = ChestXrayDataset(
+        config=config_dict,
+        phase="val",
+        crop=True,
+        patch_size=config_dict["patch_size"],
+    )
     # test_dataset = ChestXrayTestDataset(crop=True, patch_size=PATCH_SIZE, n_per_image=N_PATCHES)
 
     train_dataloader = DataLoader(train_dataset, batch_size=config_dict["batch_size"], shuffle=True)
     val_dataloader = DataLoader(val_dataset, batch_size=config_dict["batch_size"])
     predict_dataloader = DataLoader(val_dataset, batch_size=1)
 
-    config = Config(
-        batch_size=config_dict["batch_size"],
-        patch_size=config_dict["patch_size"],
-        max_epochs=config_dict["max_epochs"],
-        learning_rate=config_dict["learning_rate"],
-        weight_decay=config_dict["weight_decay"],
-        n_patches=config_dict["n_patches"],
-        len_train_dataset=len(train_dataloader),
-        no_val=config_dict["no_val"],
-        random_erasing=config_dict["random_erasing"],
-        prob=config_dict["probability"],
-        box_size=config_dict["box_size"],
+    config_dict["len_train_dataset"] = len(train_dataloader)
+    trainer = Trainer.from_argparse_args(
+        args=trainer_args,
+        callbacks=get_callbacks(config=config_dict),
+        max_steps=5 if config_dict["no_val"] else -1,
     )
-    trainer = Trainer.from_argparse_args(args, callbacks=get_callbacks())
     model = Resnet(
-        out_features=config_dict["out_features"], config=config, resnet_version=config_dict["resnet_version"]
+        out_features=config_dict["out_features"],
+        config=config_dict,
+        resnet_version=config_dict["resnet_version"],
     )
     trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
     model.save_configs(log_dir=trainer.log_dir)
@@ -92,7 +72,7 @@ def run_resnet() -> None:
         output = trainer.predict(
             model,
             predict_dataloader,
-            ckpt_path="best" if not NO_VAL else None,
+            ckpt_path="best" if not config_dict["no_val"] else None,
         )
         outputs.append(output)
         i += 1
